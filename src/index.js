f8ed1c0e1b32569c94cd4d9e80f5390dc5fcc86b
sr1994lu <sr1994lu@gmail.com>
2017-07-07 16:04:00 +0900

    build(src): render
    コンポーネント – React入門

--- src/index.js
+++ src/index.js
@@ -4,14 +4,5 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-const props = {
-  id: 'bar',
-  className: 'foo',
-}
-ReactDOM.render(
-  <h1 style={{color: 'red'}} {...props}>
-    {/* comment */}Hello, world!
-  </h1>,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
